6a68b6fcadb049e4a05049a76e28fb6b9cf62147
Sushrin <sushrinstha@gmail.com>
2025-12-18 14:20:09 +0545

    Initial Commit

---
+++ index.js
@@ -0,0 +1,115 @@
+// const weight = 106
+                                             
+// if(weight>100){
+
+//     console.log('fat')
+// }
+// else{
+//     console.log('slim')
+// }
+
+
+// for (let i=1 ; i<=10;i++)
+// {
+//  if (i%2==0){
+//      console.log(i,"even")
+
+//     }
+//     else{
+//      console.log(i,"odd")
+
+//     }
+// } 
+ 
+
+// for(i=1; i<=5;i++){
+//     console.log("*".repeat(i))
+// }
+// for(i=1; i<=5;i++){
+//     console.log("*".repeat(i))
+// }
+// for(i=1; i<=3;i++){
+//     console.log("*")
+// }
+// pop
+// const arr=[1,2,3,4]
+// // arr.pop()
+// // console.log(arr)
+// console.log(arr.pop())
+
+
+// const arr=[1,2,3,4]
+// // arr.push(5)
+// // console.log(arr)
+// console.log(arr.push(5))
+// console.log(arr)
+
+// const arr=[1,2,3,4]
+// // arr.shift()
+// // console.log(arr)
+// console.log(arr.shift())
+
+// 
+
+// const arr=[11,10,7,3]
+// let min=999999999;
+// for(let i=0; i<=arr.length-1;i++)
+// {
+    
+// if (min>arr[i]){
+//     min=arr[i]
+// }
+
+
+
+// }
+// console.log(min)
+
+
+const arr=[
+    {
+        id:1 ,
+        name: "xy",
+        age:12
+
+
+    },
+    {
+        id:2,
+        name:"yz",
+        age:2
+    },
+    {
+        id:3,
+        name:"zy",
+        age:20
+    }
+]
+// function CheckAge(age) {
+//     return age<20;
+// }
+// console.log(
+//     arr.find((a)=>a.age<20) 
+
+// )
+
+// console.log(
+//     arr.map((d,i)=> {
+//         const sqrd = d.age*d.age
+//         return{
+
+//             name:d.name,age:sqrd}
+//         }
+//     )
+
+
+// const arr=[ "mango","orange","apple","banana"]
+// arr.splice(2,1)
+// console.log(arr)
+
+// const arr=[ 1,2,3,4]
+const initialvalue = 1;
+const multiplication = arr.sort((a,b)=>b.age-a.age);
+console.log(multiplication)
+
+
